f78b6dd862964d92c1f2288d5a5e807edb095461
Camilo Rafael Pérez Chaves <camilorafaelperezchaves@gmail.com>
2026-08-30 21:31:59 -0500

    fix: change npm ci to npm install

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Requerido si despliegas en un repositorio de usuario/proyecto (ej. usuario.github.io/nombre-repo)
-  // basePath: '/nombre-de-tu-repositorio', 
   images: {
-    unoptimized: true, // GitHub Pages no soporta la optimización por servidor de Next Image
+    unoptimized: true,
   },
 };
 
